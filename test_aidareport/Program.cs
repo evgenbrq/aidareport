b91de978e1c8ead934dafcfa29e0ccac2839db16
admin <admin@DESKTOP-8D38T2B>
2022-02-14 09:38:59 +0500

    Добавьте файлы проекта.

---
+++ test_aidareport/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_aidareport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Введите путь txt файла для сохранения списка ПК. Пример:C:\Users\admin\Desktop\base\base.txt");
+            string path2 = Console.ReadLine();
+            Console.WriteLine(@"Введите путь папки с отчетами. Файлы отчетов формата <number>.htm");
+            string path = Console.ReadLine();
+            path = path.IndexOf("\\", (path.Length - 1)) != -1 ? path : path+="\\";
+
+            string[] pa = Directory.GetFiles(path);
+            foreach(string s in pa)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            for (int i = 1; i < 300; i++)
+            {
+                string path3 = path + $"{i}.htm";
+                //bool b = TryBool(path);
+
+                try
+                {
+                    var encoding = Encoding.GetEncoding(1251);
+                    using (StreamReader sr = new StreamReader(path3, encoding: encoding))
+                    {
+                        string str = sr.ReadToEnd();
+                        //Console.WriteLine(str);
+                        int x = str.IndexOf("<TD>Компьютер&nbsp;&nbsp;<TD>");
+                        int cv = str.IndexOf("<TR><TD><TD><TD>Генератор&nbsp;");
+                        string df = str.Substring(x+29, cv-x-29);
+                        using (StreamWriter sw = new StreamWriter(path2, true, Encoding.Default))
+                        {
+                            sw.Write($"u{i}:" + "    " + df);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    using (StreamWriter sw = new StreamWriter(path2, true, Encoding.Default))
+                    {
+                        sw.Write("");
+                        sw.WriteLine($"u{i} отсутствует");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ test_aidareport/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("test_aidareport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test_aidareport")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b1eea56d-8d06-4c86-a3b4-aedbbe3a840e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
